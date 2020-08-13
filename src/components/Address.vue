3c6631f83513a414b7ddd60748b1ef8830994a90
annableeker <annasdoggrooming@techcarpenter.org>
2020-08-12 23:20:19 -0400

    fix address component query error

--- src/components/Address.vue
+++ src/components/Address.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="address-block">
       <p><strong>Address:</strong></p>
-      <p>{{$static.json.address1}}</p>
-      <p>{{$static.json.address2}}</p>
+      <p>{{$static.json.address.address1}}</p>
+      <p>{{$static.json.address.address2}}</p>
   </div>
 </template>
 
 <static-query>
 query {
   json (id: "0") {
-    address1
-    address2
+    address {
+      address1
+      address2
+    }
   }
 }
 </static-query>
